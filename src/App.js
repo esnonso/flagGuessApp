b30ed37a2345626dc55f458febd99d92b129f090
esnonso <devnellynonso@gmail.com>
2021-05-04 11:51:51 +0100

    Add Source files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class FlagGuessApp extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h1>Country Flag Guessing App</h1>
+      </div>
+    )
+  }
+}
+
+export default FlagGuessApp;
